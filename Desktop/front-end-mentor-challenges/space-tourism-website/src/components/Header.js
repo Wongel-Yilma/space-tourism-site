b0b7ea543c1743dff5c06300098661be1ec81be0
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-10-04 17:00:03 +0300

    Implement hover state for the header links

--- Desktop/front-end-mentor-challenges/space-tourism-website/src/components/Header.js
+++ Desktop/front-end-mentor-challenges/space-tourism-website/src/components/Header.js
@@ -10,16 +10,20 @@ const Header = ({ page, setPage }) => {
         <div className="dash"></div>
       </div>
       <div className="header">
-        <Link to="/" onClick={() => setPage(0)}>
+        <Link className="nav__link" to="/" onClick={() => setPage(0)}>
           00 Home
         </Link>
-        <Link to="/destination" onClick={() => setPage(1)}>
+        <Link
+          className="nav__link"
+          to="/destination"
+          onClick={() => setPage(1)}
+        >
           01 Destination
         </Link>
-        <Link to="/crew" onClick={() => setPage(2)}>
+        <Link className="nav__link" to="/crew" onClick={() => setPage(2)}>
           02 Crew
         </Link>
-        <Link to="/technology" onClick={() => setPage(3)}>
+        <Link className="nav__link" to="/technology" onClick={() => setPage(3)}>
           03 Technology
         </Link>
       </div>
